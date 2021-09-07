12ddf432673c00a7f9afd5ea589fd1b9f56847fc
Matic Babnik <matic.babnik@outlook.com>
2021-09-07 06:41:32 +0200

    ...

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import needle from 'needle'
 import { JSDOM } from 'jsdom'
+import settings from './settings.json'
 
 const apiAddress = 'https://www.easistent.com/urniki/ajax_urnik';
 const headers = { user_agent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'};
 
-async function Scrape(razred: number, sola: number, week: number | undefined) {
+async function Scrape(razred: number, sola: number, week?: number) {
     try {
         const formData: any = {
             "id_sola": sola,
@@ -19,11 +20,15 @@ async function Scrape(razred: number, sola: number, week: number | undefined) {
             formData['teden'] = week;
 
         const response = await needle('post', apiAddress, formData, headers);
+        const timetable: HTMLTableElement | null = new JSDOM(response.body).window.document.querySelector('table.ednevnik-seznam_ur_teden');
+        if (!timetable) return { error: "Selector 'table.ednevnik-seznam_ur_teden' failed" };
 
-        const timetable: HTMLTableElement | null = new JSDOM(response.body).window.document.querySelector('#seznam_ur_teden > table');
-        if (!timetable) return { error: "Selector '#seznam_ur_teden > table' failed" };
+        //@ts-ignore
+        const timerangeElements: Array<HTMLDivElement>  = [...timetable.querySelectorAll('.ednevnik-seznam_ur_teden-ura>.text10.gray')];
+        console.log(timerangeElements.map(x=>x.innerHTML.split(" - ")));
+        //timerangeElements.map(x=>);
 
-
+        console.log(parseTable(timetable));
 
 
 
@@ -35,9 +40,11 @@ async function Scrape(razred: number, sola: number, week: number | undefined) {
 }
 
 function parseTable(table: HTMLTableElement) {
-
+    console.log(table);
 }
 
+
+Scrape(settings.classes.R3A, settings.school.id).then(x=>console.log(x)).catch(e=>console.error(e));
 
 const timetable: Timetable = {
     schoolId: 182,
