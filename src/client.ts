68a84be0b387732edb03cf3a7b539a8ddfab39cc
Matic Babnik <matic.babnik@outlook.com>
2025-04-14 19:23:35 +0200

    feat: Added test client

---
+++ src/client.ts
@@ -0,0 +1,19 @@
+/**
+ * Quick and dirty client for testing
+ */
+import { WeekData } from "./easistent/school";
+
+const r = await fetch("http://localhost:3000/week");
+const w = (await r.json()) as { week: WeekData };
+
+console.log(
+    w.week.events.filter(
+        (x) =>
+            x.classroomKey == "107" &&
+            x.dayIndex == 0 &&
+            x.periodIndex > 4 &&
+            x.periodIndex < 9
+    )
+);
+
+export {};
